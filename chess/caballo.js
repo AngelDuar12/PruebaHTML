c2b01d2b9dfef5bb641e458259fe48fdb3cd8f28
Arturo Duarte <marturo77@hotmail.com>
2021-12-08 16:35:22 -0500

    Ajuste inicial del caballo

---
+++ chess/caballo.js
@@ -0,0 +1,18 @@
+
+// Json
+var caballo =
+{
+    // Atributos
+    pos:'A2',
+
+    Init:function()
+    {
+      
+    },
+
+    // Metodos
+    Mover: function (nuevaPosicion) {       
+       
+        console.log('el caballo se ha movido a la posicion:'+nuevaPosicion);
+    }    
+}
