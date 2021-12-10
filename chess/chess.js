7445e3efa70c8df1f36e55a93e25fd95774bd441
Arturo Duarte <marturo77@hotmail.com>
2021-12-10 05:16:34 -0500

    Ajustes iniciales

--- chess/chess.js
+++ chess/chess.js
@@ -2,6 +2,35 @@
 var board = {
 
     table: new Array(),
+    piezas: [
+        'white-torre-izq',
+        'white-caballo-izq',
+        'white-alfil-izq',
+        'white-reina',
+        'white-rey',
+        'white-peon-1'
+    ],
+
+    posicionesIniciales: [
+        { ficha: 'white-torre-izq', posicion: 'a8' },
+        { ficha: 'white-caballo-izq', posicion: 'b8' },
+        { ficha: 'white-alfil-izq', posicion: 'c8' },
+        { ficha: 'white-reina', posicion: 'd8' },
+        { ficha: 'white-rey', posicion: 'e8' },
+        { ficha: 'white-alfil-der', posicion: 'f8' },
+        { ficha: 'white-caballo-der', posicion: 'g8' },
+        { ficha: 'white-torre-der', posicion: 'h8' },
+
+        { ficha: 'white-peon-1', posicion: 'a7' },
+        { ficha: 'white-peon-2', posicion: 'b7' },
+        { ficha: 'white-peon-3', posicion: 'c7' },
+        { ficha: 'white-peon-4', posicion: 'd7' },
+        { ficha: 'white-peon-5', posicion: 'e7' },
+        { ficha: 'white-peon-6', posicion: 'f7' },
+        { ficha: 'white-peon-7', posicion: 'g7' },
+        { ficha: 'white-peon-8', posicion: 'h7' },
+
+    ],
 
     Initialize: function() {
 
@@ -11,7 +40,7 @@ var board = {
         var margin = 40;
         var width = $('.container .board').width();
         var delta = (width / 8);
-
+        var adjustInCenter = delta / 8;
 
         for (j = 8; j >= 1; j--) {
 
@@ -19,22 +48,23 @@ var board = {
                 var nombre = absisa[i] + j;
                 var celda = {
                     name: nombre,
-                    left: i * delta + margin,
-                    top: width - (j - 1) * delta - margin
+                    left: i * delta + margin + adjustInCenter,
+                    top: width - (j - 1) * delta - margin + adjustInCenter
                 };
 
                 this.table.push(celda);
             }
         }
 
-        // Colocar las fichas en la posicion correcta
-        this.Move('torre', 'a8');
-        this.Move('caballo', 'b8');
-        this.Move('alfil', 'c8');
-        this.Move('reina', 'd8');
-        this.Move('rey', 'e8');
-        this.Move('peon', 'a7');
+        this.Reset();
 
+    },
+
+    Reset: function() {
+
+        $.each(this.posicionesIniciales, function(index, value) {
+            this.Move(value.ficha, value.posicion);
+        }.bind(this));
     },
 
     // Mover una ficha en el tablero
