2569db64c14245e4671e4b9b6b01768ba7917645
Arturo Duarte <marturo77@hotmail.com>
2021-12-10 16:24:21 -0500

    Movimiento del caballo

--- chess/caballo.js
+++ chess/caballo.js
@@ -2,15 +2,28 @@ var caballo = {
     Mover: function() {
 
         var ficha = 'blancas-caballo-izq';
+        var actual = this.MostrarPosicion(ficha);
 
-        this.MostrarPosicion(ficha);
-        board.Move(ficha, 'c6');
-        this.MostrarPosicion(ficha);
-        board.Move(ficha, 'a5');
-        this.MostrarPosicion(ficha);
-        board.Move(ficha, 'b3');
-        this.MostrarPosicion(ficha);
-        this.MostrarPosicion(ficha);
+        var values = Array.from(actual.posicion);
+        var letra = values[0];
+        var numero = parseInt(values[1]);
+
+        // b8 => a6 // Bajo una letra y salto dos casillas a la izq
+        var destino1 = 'a' + (numero - 2);
+        var destino2 = 'c' + (numero - 2);
+        var destinos = [destino1, destino2];
+
+        $.each(destinos, function(index, pos) {
+            board.Move(ficha, pos);
+        }.bind(this));
+
+
+        // board.Move(ficha, 'c6');
+        // actual = this.MostrarPosicion(ficha);
+        // board.Move(ficha, 'a5');
+        // actual = this.MostrarPosicion(ficha);
+        // board.Move(ficha, 'b3');
+        // actual = this.MostrarPosicion(ficha);
 
         // Cuales serian las posibles posiciones a las que se 
         // puede mover un caballo
@@ -18,11 +31,14 @@ var caballo = {
     },
 
     MostrarPosicion: function(ficha) {
-        var posicion = board.posicionesIniciales.find(x => x.name == ficha);
+        var posicion = board.posicionesIniciales.find(x => x.ficha == ficha);
 
         if (posicion) {
-            console.log(ficha + ' esta en la posicion actual:' + posicion);
+
+            return posicion;
         }
+
+        return null;
     }
 }
 
