d172d8a28d3fe8e879c06c3d2f48521f0ff0746d
AngelDuar12 <santiangelduarte@gmail.com>
2021-12-10 19:33:36 -0500

    js del alfil

---
+++ chess/alfil.js
@@ -0,0 +1,35 @@
+var alfil = {
+    Mover: function() {
+
+        var ficha = 'blancas-alfil-izq';
+        var actual = this.MostrarPosicion(ficha);
+
+        var values = Array.from(actual.posicion);
+        var letra = values[0];
+        var numero = parseInt(values[1]);
+
+        var destino1 = 'a' + (numero - 2);
+        var destino2 = 'c' + (numero - 2);
+        var destinos = [destino1, destino2];
+
+        $.each(destinos, function(index, pos) {
+            board.Move(ficha, pos);
+        }.bind(this));
+
+    },
+
+    MostrarPosicion: function(ficha) {
+        var posicion = board.posicionesIniciales.find(x => x.ficha == ficha);
+
+        if (posicion) {
+
+            return posicion;
+        }
+
+        return null;
+    }
+}
+
+function Mover_Alfil() {
+    alfil.Mover();
+}
