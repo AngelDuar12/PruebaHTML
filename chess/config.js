ff97b44ea4c81c6e838fd705083373982c60fac8
Arturo Duarte <marturo77@hotmail.com>
2021-12-12 12:47:14 -0500

    Ajuste require

--- chess/config.js
+++ chess/config.js
@@ -3,5 +3,14 @@ requirejs.config({
     paths: {
 
         jquery: 'jquery/dist/jquery.min',
+        chess: 'chess'
+    },
+    shim: {
+        'app': ['jquery', 'chess'],
     }
+
+
 });
+
+// Inicia la aplicacion
+require(['app']);
