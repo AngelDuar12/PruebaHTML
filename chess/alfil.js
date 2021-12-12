202655fbe70495033d2547dcc9cc01efbbf36d2d
AngelDuar12 <santiangelduarte@gmail.com>
2021-12-12 11:18:22 -0500

    Alfil configurado

--- chess/alfil.js
+++ chess/alfil.js
@@ -8,9 +8,12 @@ var alfil = {
         var letra = values[0];
         var numero = parseInt(values[1]);
 
-        var destino1 = 'a' + (numero - 2);
-        var destino2 = 'b' + (numero - 1);
-        var destinos = [destino1, destino2];
+        var destino1 = 'b' + (numero - 1);
+        var destino2 = 'a' + (numero - 2);
+        var destino3 = 'b' + (numero - 3);
+        var destino4 = 'c' + (numero - 4);
+        var destino5 = 'd' + (numero - 5); 
+        var destinos = [destino1, destino2, destino3, destino4, destino5];
 
         $.each(destinos, function(index, pos) {
             board.Move(ficha, pos);
