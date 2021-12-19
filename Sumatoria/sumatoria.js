b81175bf6d5aa4b7bfd3908d58073daf39b7d735
Arturo Duarte <marturo77@hotmail.com>
2021-12-19 18:33:35 -0500

    Suma 10 numeros

--- Sumatoria/sumatoria.js
+++ Sumatoria/sumatoria.js
@@ -1,24 +1,14 @@
 var uno = 1;
-
 var dos = 2;
-
 var tres = 3;
-
 var cuatro = 4;
-
 var cinco = 5;
-
 var seis = 6;
-
 var siete = 7;
-
 var ocho = 8;
-
 var nueve = 9;
-
 var diez = 10;
 
-var diez
-
-
-
+var resultado = uno + dos + tres + cuatro + cinco + seis + siete + ocho + nueve + diez;
+var parrafo = document.getElementById('valor');
+parrafo.innerText = resultado;
