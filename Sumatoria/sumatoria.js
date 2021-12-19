ed61b954895683d3916d4c77e91493ec4c875beb
AngelDuar12 <santiangelduarte@gmail.com>
2021-12-19 18:25:39 -0500

    sumatoria

---
+++ Sumatoria/sumatoria.js
@@ -0,0 +1,24 @@
+var uno = 1;
+
+var dos = 2;
+
+var tres = 3;
+
+var cuatro = 4;
+
+var cinco = 5;
+
+var seis = 6;
+
+var siete = 7;
+
+var ocho = 8;
+
+var nueve = 9;
+
+var diez = 10;
+
+var diez
+
+
+
